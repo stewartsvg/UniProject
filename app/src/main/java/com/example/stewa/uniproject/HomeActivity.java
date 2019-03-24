7014ff71f888345c3e910014fb29324517881da4
stewartsvg <44290572+stewartsvg@users.noreply.github.com>
2019-03-24 20:49:12 +0000

    Added home page and buttons, added navigation bar buttons and icons, prevented ability for users to go back to home page after logging out.

--- app/src/main/java/com/example/stewa/uniproject/HomeActivity.java
+++ app/src/main/java/com/example/stewa/uniproject/HomeActivity.java
@@ -1,8 +1,15 @@
 package com.example.stewa.uniproject;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,7 +18,24 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Button logoutButton = (Button) findViewById(R.id.btn_home_log_out);
 
-        getWindow().setNavigationBarColor(Color.parseColor("#20111111"));
+        //empties credentials and returns to login screen
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("PREFS", MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                //sets credentials to be empty when user logs out
+                editor.putString("credentials", "");
+                editor.commit();
+
+                Intent goToLoginScreen = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(goToLoginScreen);
+                finish();
+
+            }
+        });
     }
 }
